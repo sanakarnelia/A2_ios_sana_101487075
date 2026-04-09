ca0a9cddcaad53151094f3c28cab0f5941322bbb
sanakarnelia <sana.karnelia@gmail.com>
2026-04-08 23:33:00 -0400

    Connect UI to Code

--- A2_ios_sana_101487075/ViewController.swift
+++ A2_ios_sana_101487075/ViewController.swift
@@ -6,8 +6,23 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var providerLabel: UILabel!
+
+    @IBOutlet weak var searchBar: UISearchBar!
+
+    var products: [Product] = []
+    var filteredProducts: [Product] = []
+    var currentIndex = 0
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
