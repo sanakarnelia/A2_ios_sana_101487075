a174b68388fd62135d5ea52baea4d6c3ca1640c6
sanakarnelia <sana.karnelia@gmail.com>
2026-04-09 14:00:57 -0400

    connect class Addproductviewcontroller

---
+++ A2_ios_sana_101487075/AddProductViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddProductViewController.swift
+//  A2_ios_sana_101487075
+//
+//  Created by Maaz Karnelia on 2026-04-09.
+//
+
+import CoreData
+import UIKit
+
+class AddProductViewController: UIViewController {
+    
+}
